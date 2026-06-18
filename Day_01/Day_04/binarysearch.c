00ff10901a0c07431c05353d02546c42d2770889
khushimehrotra <khushimehrotra2401@gmail.com>
2026-06-18 18:36:12 +0530

    Day 4

---
+++ Day_01/Day_04/binarysearch.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int binarySearch(int arr[], int n, int target){
+    int left =0, right = n-1;
+    while(left<=right){
+        int mid = left +(right - left)/2;
+        if(arr[mid]== target){
+            return mid;
+        }else if(arr[mid]< target){
+            left = mid + 1;
+        }else{
+            right = mid-1;
+        }        
+    }
+    return -1;
+}
+int main(){
+    int arr[] = {1,3,5,7,9,11,13,15};
+    int n=8;
+    int target = 7;
+    int result = binarySearch(arr,n, target);
+    if(result !=-1){
+        printf("Target found at %d\n", result);
+    } else {
+        printf("Target not found\n");
+    }
+    return 0;
+}
