53746d3bb83463d2d243cae32eec698255a4f4e5
khushimehrotra <khushimehrotra2401@gmail.com>
2026-06-20 23:26:38 +0530

    day6

--- Day_01/Day_04/Problem_2.py
+++ Day_01/Day_04/Problem_2.py
@@ -1,2 +1,16 @@
 def reverse(nums,start,end):
-    while start <end:
+    while start < end:
+        nums[start], nums[end] = nums[end], nums[start]
+        start +=1
+        end -=1
+
+def rotate(nums, k):
+    n = len(nums)
+    k=k%n
+    reverse(nums,0,n-1)
+    reverse(nums,0,k-1)
+    reverse(nums,k,n-1)
+
+arr1 = [1,2,3,4,5,6,7]
+rotate(arr1,3)
+print(arr1)
